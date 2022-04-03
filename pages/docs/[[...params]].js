1900edde3a925e1a5f448627407765277516ca74
hicoderindia <hicoder.dolly@gmail.com>
2022-04-03 21:22:49 +0530

    Implementation of catch all Routes

---
+++ pages/docs/[[...params]].js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const docs = () => {
+  const router = useRouter();
+  const { params =[]} = router.query;
+  console.log(params);
+
+  if (params.length === 2) {
+    return (
+      <p>
+        {" "}
+        viewing a docs for feature: {params[0]} and a consept: {params[1]}
+      </p>
+    );
+  } else if (params.length === 1) {
+    return <p> viewing docs for feature: {params[0]}</p>;
+  }
+
+  return <div>This is form DOCS</div>;
+};
+
+export default docs;
